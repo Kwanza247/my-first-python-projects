cadf6603a4ece9e3818e5cf94b3095112fb6c2ed
Kwanza247 <Ibrahimadegboye60@gmail.com>
2025-06-02 15:15:54 +0100

    Update todo_list.py

--- todo_list.py
+++ todo_list.py
@@ -1,1 +1,16 @@
+# My First Python Projects
+
+This repository contains basic Python scripts I've built as part of my journey to become a Backend Developer.
+
+## Projects Included
+
+- `calculator.py` – A simple calculator for basic arithmetic.
+- `todo_list.py` – A command-line To-Do list app.
+- `number_guess_game.py` – A small number guessing game using random numbers.
+
+These are my early projects as I continue to learn Python, Django, and backend development.
+
+## 🔗 Connect with Me
+- [LinkedIn](linkedin.com/in/ibrahimalaniadegboye)  
+- [ALX Virtual Assistant Certified]  
 
